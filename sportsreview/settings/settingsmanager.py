1b1e7d2cc34bc53d12723a4f06cfac3d5fbcd6d2
Andrew Robinson <andrewjrobinson@gmail.com>
2014-04-18 15:36:09 +1000

    Fixed a typo on licence headers

--- sportsreview/settings/settingsmanager.py
+++ sportsreview/settings/settingsmanager.py
@@ -14,7 +14,7 @@
 #  *  GNU Lesser General Public License for more details.                        *
 #  *                                                                             *
 #  *  You should have received a copy of the GNU Lesser General Public License   *
-#  *  along with QualityTrim.  If not, see <http://www.gnu.org/licenses/>.       *
+#  *  along with SportsReview.  If not, see <http://www.gnu.org/licenses/>.      *
 #  *                                                                             *
 #  *******************************************************************************/
 import types
