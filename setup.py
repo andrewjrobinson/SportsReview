0d7f749b05f013ae3745299ee278a8a83e325ed9
Andrew Robinson <andrewjrobinson@gmail.com>
2014-04-18 15:36:32 +1000

    First version of setup script

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# /*******************************************************************************
+#  *   (c) Andrew Robinson (andrewjrobinson@gmail.com) 2014                      *
+#  *                                                                             *
+#  *  This file is part of SportsReview.                                         *
+#  *                                                                             *
+#  *  SportsReview is free software: you can redistribute it and/or modify       *
+#  *  it under the terms of the GNU Lesser General Public License as published   *
+#  *  by the Free Software Foundation, either version 3 of the License, or       *
+#  *  (at your option) any later version.                                        *
+#  *                                                                             *
+#  *  SportsReview is distributed in the hope that it will be useful,            *
+#  *  but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+#  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+#  *  GNU Lesser General Public License for more details.                        *
+#  *                                                                             *
+#  *  You should have received a copy of the GNU Lesser General Public License   *
+#  *  along with SportsReview.  If not, see <http://www.gnu.org/licenses/>.      *
+#  *                                                                             *
+#  *******************************************************************************/
+'''
+Created on 11/04/2014
+@author: Andrew Robinson
+'''
+
+from distutils.core import setup
+
+setup(name='SportsReview',
+      version='0.1',
+      description='Sports training analysis tool for sports that require precise movements, swings, or timings',
+      author='Andrew Robinson',
+      author_email='andrewjrobinson@gmail.com',
+      url='http://andrewjrobinson.github.io/SportsReview/',
+      packages=['sportsreview'],
+     )
